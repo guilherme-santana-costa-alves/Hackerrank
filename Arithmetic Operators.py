9b83963563b44f1523b2fa6438d6fa573b018416
sweetpanqueca <53286067+sweetpanqueca@users.noreply.github.com>
2022-07-11 19:09:03 -0300

    Create Arithmetic Operators.py

---
+++ Arithmetic Operators.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    soma = a + b
+    menos = a - b
+    produto = a * b
+    print(soma)
+    print(menos)
+    print(produto)
